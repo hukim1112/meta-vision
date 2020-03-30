4f1a460e075dc6bf203d4b9a7f0637a362d28e6c
dan <hyounguk1112@gmail.com>
2020-03-30 23:44:24 +0900

    update

--- CNNAlign/developing.py
+++ CNNAlign/developing.py
@@ -1,4 +1,6 @@
-import os, json, argparse
+import os
+import json
+import argparse
 from data_loader import load_data
 from models.cnn_geo import CNN_geo
 from models.modules import Feature_Extractor, Correlation_network, Spatial_transformer_regressor
@@ -10,6 +12,7 @@ import cv2
 import numpy as np
 from utils import image
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
 
 def loss_fn(pred, label):
     return tf.reduce_sum(tf.keras.losses.MSE(pred, label))
@@ -44,6 +47,9 @@ def main():
     with open(config) as file:
         config = json.load(file)
 
+        log_dir = 'logs/train'
+    summary_writer = tf.summary.create_file_writer(log_dir)
+
     batch_size = config['train']['batch_size']
     splits = ['train', 'val']
     datasets = load_data(splits, config)
@@ -61,22 +67,29 @@ def main():
     for epoch in range(epochs):
         print("start of epoch {}".format(epoch + 1))
         for step, (image_a, image_b, label) in enumerate(train_ds):
-            pred, loss = train_step(image_a, image_b, label, model, optimizer)
-            train_loss(loss)
+            pred, t_loss = train_step(
+                image_a, image_b, label, model, optimizer)
+            train_loss(t_loss)
             if step % 100 == 0:
                 print('Training loss (for one batch) at step {}: {}'.format(
                     step, loss.numpy()))
         for image_a, image_b, label in val_ds:
-            pred, loss = val_step(image_a, image_b, label, model)
-            val_loss(loss)
+            pred, v_loss = val_step(image_a, image_b, label, model)
+            val_loss(v_loss)
         template = 'Epoch {}, Loss: {}, ' \
                    'Val Loss: {}'
         print(template.format(epoch + 1, train_loss.result(), val_loss.result()))
         print("end of epoch.")
+        with summary_writer.as_default():
+            tf.summary.scalar(
+                'train_loss', train_loss.result(), step=epoch + 1)
+            tf.summary.scalar('val_loss', val_loss.result(), step=epoch + 1)
+            summary_writer.flush()
         train_loss.reset_states()
         val_loss.reset_states()
 
-        if epoch == 0 or (epoch + 1) % 20 == 0:
+        # Save your model
+        if epoch == 0 or (epoch + 1) % 5 == 0:
             model.save_weights(os.path.join(
                 config['checkpoint_dir'], config['model_name'] + "_{}.h5".format(epoch + 1)))
 
