ec19cbde6d6d830aadfe0c4b8059b173e616d353
hukim <hukim@artiasolution.com>
2020-04-13 02:22:06 +0900

    update

--- CNNAlign/train_CNNgeo.py
+++ CNNAlign/train_CNNgeo.py
@@ -75,11 +75,11 @@ def main():
             if step % 20 == 0:
                 print('Training loss (for one batch) at step {}: {}'.format(
                     step, t_loss.numpy()))
-        # for image_a, image_b, label in val_ds:
-        #     pred, v_loss = val_step(image_a, image_b, label, model)
-        #     val_loss(v_loss)
-        # template = 'Epoch {}, Loss: {}, Val Loss: {}'
-        # print(template.format(epoch + 1, train_loss.result(), val_loss.result()))
+        for image_a, image_b, label in val_ds:
+            pred, v_loss = val_step(image_a, image_b, label, model)
+            val_loss(v_loss)
+        template = 'Epoch {}, Loss: {}, Val Loss: {}'
+        print(template.format(epoch + 1, train_loss.result(), val_loss.result()))
         print("end of epoch.")
         with summary_writer.as_default():
             tf.summary.scalar(
@@ -87,7 +87,7 @@ def main():
             tf.summary.scalar('val_loss', val_loss.result(), step=epoch + 1)
             summary_writer.flush()
         # Save your model
-        saver.save_or_not(model, epoch + 1, train_loss.result())
+        saver.save_or_not(model, epoch + 1, val_loss.result())
         train_loss.reset_states()
         val_loss.reset_states()
     print("Checkpoint directory : ", ckpt_dir)
