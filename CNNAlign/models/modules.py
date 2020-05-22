a056d9eb32608186b026370e8ad6d3f5b7219838
dan <hyounguk1112@gmail.com>
2020-05-22 21:29:42 +0900

    update

--- CNNAlign/models/modules.py
+++ CNNAlign/models/modules.py
@@ -44,12 +44,12 @@ class Correlation_network(tf.keras.layers.Layer):
         feature_B = feature_B[:, tf.newaxis, tf.newaxis, :, :, :]
         # correlation score has tensor shape as [batch, HA, WA, HB, WB]
         corr_score = tf.reduce_mean(tf.multiply(feature_A, feature_B), axis=-1)
-        
+        '''
         ambiguous_match_penalty = tf.math.sqrt(
             tf.reduce_sum(tf.pow(corr_score, 2), axis=[3, 4], keepdims=True))
         return tf.math.divide(corr_score, ambiguous_match_penalty+0.00001)
         # see eq (2) in "End-to-end weakly-supervised semantic alignment"
-
+        '''
         return corr_score
 
 
