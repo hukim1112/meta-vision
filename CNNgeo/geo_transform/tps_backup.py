bb2bd6269cae5a32f2338f9ac1e4282c746774aa
dan <hyounguk1112@gmail.com>
2020-07-28 15:46:20 +0900

    update

--- CNNgeo/geo_transform/tps_backup.py
+++ CNNgeo/geo_transform/tps_backup.py
@@ -2,9 +2,11 @@ import tensorflow as tf
 import numpy as np
 
 
-def ThinPlateSpline(coord, vector, out_size):
+def ThinPlateSpline(U, coord, vector, out_size):
     """Thin Plate Spline Spatial Transformer Layer
     TPS control points are arranged in arbitrary positions given by `coord`.
+    U : float Tensor [num_batch, height, width, num_channels].
+      Input Tensor.
     coord : float Tensor [num_batch, num_point, 2]
       Relative coordinate of the control points.
     vector : float Tensor [num_batch, num_point, 2]
@@ -25,6 +27,68 @@ def ThinPlateSpline(coord, vector, out_size):
         rep = tf.cast(rep, 'int32')
         x = tf.matmul(tf.reshape(x, (-1, 1)), rep)
         return tf.reshape(x, [-1])
+
+    def _interpolate(im, x, y, out_size):
+        # constants
+        num_batch = tf.shape(im)[0]
+        height = tf.shape(im)[1]
+        width = tf.shape(im)[2]
+        channels = tf.shape(im)[3]
+
+        x = tf.cast(x, 'float32')
+        y = tf.cast(y, 'float32')
+        height_f = tf.cast(height, 'float32')
+        width_f = tf.cast(width, 'float32')
+        out_height = out_size[0]
+        out_width = out_size[1]
+        zero = tf.zeros([], dtype='int32')
+        max_y = tf.cast(tf.shape(im)[1] - 1, 'int32')
+        max_x = tf.cast(tf.shape(im)[2] - 1, 'int32')
+
+        # scale indices from [-1, 1] to [0, width/height]
+        x = (x + 1.0) * (width_f) / 2.0
+        y = (y + 1.0) * (height_f) / 2.0
+
+        # do sampling
+        x0 = tf.cast(tf.floor(x), 'int32')
+        x1 = x0 + 1
+        y0 = tf.cast(tf.floor(y), 'int32')
+        y1 = y0 + 1
+
+        x0 = tf.clip_by_value(x0, zero, max_x)
+        x1 = tf.clip_by_value(x1, zero, max_x)
+        y0 = tf.clip_by_value(y0, zero, max_y)
+        y1 = tf.clip_by_value(y1, zero, max_y)
+        dim2 = width
+        dim1 = width * height
+        base = _repeat(tf.range(num_batch) * dim1, out_height * out_width)
+        base_y0 = base + y0 * dim2
+        base_y1 = base + y1 * dim2
+        idx_a = base_y0 + x0
+        idx_b = base_y1 + x0
+        idx_c = base_y0 + x1
+        idx_d = base_y1 + x1
+
+        # use indices to lookup pixels in the flat image and restore
+        # channels dim
+        im_flat = tf.reshape(im, tf.stack([-1, channels]))
+        im_flat = tf.cast(im_flat, 'float32')
+        Ia = tf.gather(im_flat, idx_a)
+        Ib = tf.gather(im_flat, idx_b)
+        Ic = tf.gather(im_flat, idx_c)
+        Id = tf.gather(im_flat, idx_d)
+
+        # and finally calculate interpolated values
+        x0_f = tf.cast(x0, 'float32')
+        x1_f = tf.cast(x1, 'float32')
+        y0_f = tf.cast(y0, 'float32')
+        y1_f = tf.cast(y1, 'float32')
+        wa = tf.expand_dims(((x1_f - x) * (y1_f - y)), 1)
+        wb = tf.expand_dims(((x1_f - x) * (y - y0_f)), 1)
+        wc = tf.expand_dims(((x - x0_f) * (y1_f - y)), 1)
+        wd = tf.expand_dims(((x - x0_f) * (y - y0_f)), 1)
+        output = tf.add_n([wa * Ia, wb * Ib, wc * Ic, wd * Id])
+        return output
 
     def _meshgrid(height, width, coord):
         x_t = tf.tile(
@@ -50,13 +114,17 @@ def ThinPlateSpline(coord, vector, out_size):
                          1)  # [bn, 3+pn, h*w]
         return grid
 
-    def _transform(T, coord, out_size):
+    def _transform(T, coord, input_dim, out_size):
+        num_batch = tf.shape(input_dim)[0]
+        height = tf.shape(input_dim)[1]
+        width = tf.shape(input_dim)[2]
+        num_channels = tf.shape(input_dim)[3]
+
         # grid of (x_t, y_t, 1), eq (1) in ref [1]
+        height_f = tf.cast(height, 'float32')
+        width_f = tf.cast(width, 'float32')
         out_height = out_size[0]
         out_width = out_size[1]
-        out_height_f = tf.cast(out_height, 'float32')
-        out_width_f = tf.cast(out_width, 'float32')
-
         grid = _meshgrid(out_height, out_width, coord)  # [2, h*w]
 
         # transform A x (1, x_t, y_t, r1, r2, ..., rn) -> (x_s, y_s)
@@ -64,12 +132,17 @@ def ThinPlateSpline(coord, vector, out_size):
         T_g = tf.matmul(T, grid)
         x_s = tf.slice(T_g, [0, 0, 0], [-1, 1, -1])
         y_s = tf.slice(T_g, [0, 1, 0], [-1, 1, -1])
-        x_s_meshgrid = tf.reshape(x_s, [-1, out_height_f, out_width_f])
-        y_s_meshgrid = tf.reshape(y_s, [-1, out_height_f, out_width_f])
-        x_s_meshgrid = (x_s_meshgrid + 1) * out_width_f / 2
-        y_s_meshgrid = (y_s_meshgrid + 1) * out_height_f / 2
+        x_s_flat = tf.reshape(x_s, [-1])
+        y_s_flat = tf.reshape(y_s, [-1])
 
-        return x_s_meshgrid, y_s_meshgrid
+        input_transformed = _interpolate(
+            input_dim, x_s_flat, y_s_flat, out_size)
+
+        output = tf.reshape(
+            input_transformed,
+            tf.stack([num_batch, out_height, out_width, num_channels]))
+
+        return T_g, x_s, y_s, x_s_flat, y_s_flat
 
     def _solve_system(coord, vector):
         num_batch = tf.shape(coord)[0]
@@ -98,5 +171,5 @@ def ThinPlateSpline(coord, vector, out_size):
         return T
 
     T = _solve_system(coord, vector)
-    output = _transform(T, coord, out_size)
+    output = _transform(T, coord, U, out_size)
     return output
