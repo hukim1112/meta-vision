1eadbbd2e6c4db6d4cb877fedd79d9048a344fba
dan <hyounguk1112@gmail.com>
2020-08-18 13:34:10 +0900

    update

--- CNNgeo/data_loader/dev_dataset.py
+++ CNNgeo/data_loader/dev_dataset.py
@@ -5,41 +5,42 @@ import tensorflow as tf
 from functools import partial
 from geo_transform.tf_tps import ThinPlateSpline as tps
 
-def tf_image_process(image, tps_random_rate, output_size):
-    motion_vectors = (tf.random.uniform([9, 2]) - 0.5) * 2 * tps_random_rate
-    return tf.py_function(py_image_process, [image, motion_vectors, output_size], [tf.float32, tf.float32, tf.float32])
 
-def py_image_process(image, motion_vectors, output_size):
+def pad_image(image, pad_ratio):
+    '''
+        input : original image, padding_ratio( ragne 0~1 )
+        output : padded_image(reflected boundary)
+    '''
+    original_size = (image.shape[0], image.shape[1])
+    top = bottom = int(original_size[0] * pad_ratio)
+    left = right = int(original_size[1] * pad_ratio)
+    padded_image = cv2.copyMakeBorder(
+        image, top, bottom, left, right, cv2.BORDER_REFLECT)
+    return padded_image
+
+
+def py_image_process(image, motion_vectors, tps_random_rate, output_size):
+    image = tf.keras.applications.vgg16.preprocess_input(image)
     image = image.numpy()
-    #image = preprocess_input(image)
-    control_points = tf.constant([[-1.0, -1.0], [0.0, -1.0], [1.0, -1.0],
-                               [-1.0, 0.0], [0.0, 0.0], [1.0, 0.0],
-                               [-1.0, 1.0], [0.0, 1.0], [1.0, 1.0]], dtype=tf.float32)
-    x_s, y_s = tps(control_points[tf.newaxis,::], -motion_vectors[tf.newaxis,::], output_size)
-    synth_image = cv2.remap(image, x_s[0].numpy(), y_s[0].numpy(), cv2.INTER_CUBIC)
+    tps_random_rate = tps_random_rate.numpy()
+    #image = image / 255.
+    padded_image = pad_image(image, tps_random_rate)
+
+    ratio = 1 / (1 + tps_random_rate)
+    control_points = tf.constant([[-ratio, -ratio], [0.0, -ratio], [ratio, -ratio],
+                                  [-ratio, 0.0], [0.0, 0.0], [ratio, 0.0],
+                                  [-ratio, ratio], [0.0, ratio], [ratio, ratio]], dtype=tf.float32)
+    x_s, y_s = tps(control_points[tf.newaxis, ::], -
+                   motion_vectors[tf.newaxis, ::], padded_image.shape[0:2])
+    synth_image = cv2.remap(
+        padded_image, x_s[0].numpy(), y_s[0].numpy(), cv2.INTER_CUBIC)
+    dH = (synth_image.shape[0] - image.shape[0]) / 2
+    dW = (synth_image.shape[1] - image.shape[1]) / 2
+    dH, dW = int(dH), int(dW)
+    synth_image = synth_image[dH:-dH, dW:-dW]
     return image, synth_image, motion_vectors
 
-def load_dev_dataset():
-    _datapath = "sample_dataset"
-    filelist = os.listdir(_datapath)
-    input_size = (200, 200)
 
-    images = []
-
-    for f in filelist:
-        _path = os.path.join(_datapath, f)
-        img = cv2.imread(_path)[:,:,::-1]
-        img = cv2.resize(img, input_size, interpolation=cv2.INTER_AREA)
-        images.append(img)
-
-    images = np.array(images, dtype=np.float32)
-
-    tps_random_rate = 0.4
-    output_size = (200, 200)
-    map_func = partial(tf_image_process, tps_random_rate=tps_random_rate,
-                          output_size=output_size)
-
-    ds = tf.data.Dataset.from_tensor_slices(images)
-    ds = ds.map(map_func, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds = ds.batch(16)
-    return ds
+def tf_image_process(image, tps_random_rate, output_size):
+    motion_vectors = (tf.random.uniform([9, 2]) - 0.5) * 2 * tps_random_rate
+    return tf.py_function(py_image_process, [image, motion_vectors, tps_random_rate, output_size], [tf.float32, tf.float32, tf.float32])
--- CNNgeo/data_loader/loader.py
+++ CNNgeo/data_loader/loader.py
@@ -1,11 +1,9 @@
 from .mini_imagenet import load_mini_imagenet
-from .dev import load_dev_dataset
+
 
 def load(splits, config):
     if config['dataset_name'] == 'mini_imagenet':
         ds = load_mini_imagenet(splits, config)
-    elif config['dataset_name'] == 'dev':
-        ds = load_dev_dataset(config)
     else:
         raise ValueError("Wrong dataset name : {}".format(
             config['dataset_name']))
